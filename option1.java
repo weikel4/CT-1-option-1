29fa6974444532f19f06be5b187ba29db818b2b1
weikel4 <kaleb.weikel@csuglobal.edu>
2024-07-19 16:30:43 -0500

    Add files via upload

---
+++ option1.java
@@ -0,0 +1,20 @@
+public class option1 {
+
+	public static void main(String[] args) {
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		String zip;
+		
+		firstName = "John";
+		lastName = "Doe";
+		address = "123 That Street";
+		city = "Somewhere";
+		zip = "12345";
+		
+		System.out.print(firstName + " " + lastName + " lives at: " + address + ", " + city + ", " + zip);
+
+	}
+
+}
